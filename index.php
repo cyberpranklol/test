05bcd3762a5060569d7ffe1b35c78d09e1ad3669
cyberprankyo <kljajic.stefan@yahoo.com>
2023-12-13 10:12:10 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $ime = "Stefan Kljajic"; // ovo je string
+    $godina = 27; //ovo je intiger (int)
+    $visina = 175.2; //ovo je float, decimanal broj
+    $programer = true; //ovo je boolean, $programer = 1
+
+    echo "Moje ime je $ime, imam $godina godina, visok sam $visina. ";
+
+    $visina = 185.3;
+    $ime = "Olivera Kljajic";
+    $godina = 28;
+
+    echo "Moje ime je $ime, imam $godina godina, visoka sam $visina. ";
+   
+
+
+?>
---
+++ vezba.php
@@ -0,0 +1,23 @@
+<?php
+
+   $naslov = "Postani Programer";
+   $stavke = ["Glavna", "O nama", "Kontakt"];
+   array_push ($stavke, "Info");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Postani programer</title>
+</head>
+<body>
+     <h1> <?php echo $naslov ?> </h1>
+         <nav>
+            <li> <?php echo $stavke[0] ?> </li>
+            <li> <?php echo $stavke [1] ?> </li>
+            <li> <?php echo $stavke [2] ?> </li>
+            <li> <?php echo $stavke [3] ?> </li>
+         </nav>
+</body>
+</html>
